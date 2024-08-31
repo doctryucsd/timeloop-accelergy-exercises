1fb46b76bb3fa2c9f533b978c54c7a5b6899d08e
doctryucsd <chy036@ucsd.edu>
2024-08-31 14:20:34 +0000

    hardware for different layers

--- workspace/tutorial_exercises/timeloop_ppa/timeloop_ppa.py
+++ workspace/tutorial_exercises/timeloop_ppa/timeloop_ppa.py
@@ -1,13 +1,15 @@
-import timeloopfe.v4 as tl
 import os
-from torch import nn, Tensor
-from typing import List, Dict, Tuple
-import torch
-from cimloop.workspace import results2ppa, get_run_dir
+from typing import Dict, List, Tuple
+
 import joblib
+import timeloopfe.v4 as tl
+import torch
+from cimloop.workspace import get_run_dir, results2ppa
+from torch import Tensor, nn
 
 THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 TOP_PATH = f"{THIS_SCRIPT_DIR}/top.yaml.jinja"
+
 
 def update_spec_problem(dims: List[Tuple[str, int]], spec: tl.Specification):
     COEFFECIENT_DIMS = ["Wdilation", "Hdilation", "Wstride", "Hstride"]
@@ -23,13 +25,19 @@ def update_spec_problem(dims: List[Tuple[str, int]], spec: tl.Specification):
         if coeff_name in coefficients:
             coeff["default"] = coefficients[coeff_name]
 
-def update_spec_problem_instance_linear(layer: nn.Linear, input: Tensor, output: Tensor) -> List[Tuple[str, int]]:
+
+def update_spec_problem_instance_linear(
+    layer: nn.Linear, input: Tensor, output: Tensor
+) -> List[Tuple[str, int]]:
     return [
         ("C", layer.in_features),
         ("M", layer.out_features),
     ]
 
-def conv2d_dims(layer: nn.Conv2d, input: Tensor, output: Tensor) -> List[Tuple[str, int]]:
+
+def conv2d_dims(
+    layer: nn.Conv2d, input: Tensor, output: Tensor
+) -> List[Tuple[str, int]]:
 
     return [
         ("C", layer.in_channels),
@@ -46,6 +54,7 @@ def conv2d_dims(layer: nn.Conv2d, input: Tensor, output: Tensor) -> List[Tuple[s
         ("Q", output.shape[3]),
     ]
 
+
 def get_layer_data(model: nn.Module, input_tensor: Tensor):
     """
     Given a model and an input tensor, this function will return a dictionary that describes the problem in timeloop
@@ -59,14 +68,17 @@ def get_layer_data(model: nn.Module, input_tensor: Tensor):
         if isinstance(module, nn.Conv2d):
             layer_data.append(conv2d_dims(module, input, output))
         elif isinstance(module, nn.Linear):
-            layer_data.append(update_spec_problem_instance_linear(module, input, output))
+            layer_data.append(
+                update_spec_problem_instance_linear(module, input, output)
+            )
         else:
             return
 
     hooks = []
+
     def register_hooks(module: nn.Module):
         if len(list(module.children())) == 0:  # It's a bottom-level layer
-            hooks.append(module.register_forward_hook(hook_fn)) # type: ignore
+            hooks.append(module.register_forward_hook(hook_fn))  # type: ignore
         else:
             for submodule in module.children():
                 register_hooks(submodule)
@@ -81,7 +93,10 @@ def get_layer_data(model: nn.Module, input_tensor: Tensor):
 
     return layer_data
 
-def run_layer(layer_data: List[Tuple[str, int]], x_dim: int, y_dim: int, frequency: int):
+
+def run_layer(
+    layer_data: List[Tuple[str, int]], x_dim: int, y_dim: int, frequency: int
+):
     spec = tl.Specification.from_yaml_files(TOP_PATH)
     update_spec_problem(layer_data, spec)
     spec.architecture.find("PE").spatial.meshX = x_dim
@@ -99,20 +114,32 @@ def run_layer(layer_data: List[Tuple[str, int]], x_dim: int, y_dim: int, frequen
 
     return result
 
-def timeloop_ppa(model: nn.Module, x_test: Tensor, x_dim: int, y_dim: int, frequency: int, cell_bit: int):
+
+def timeloop_ppa(
+    model: nn.Module,
+    x_test: Tensor,
+    x_dim_list: List[int],
+    y_dim_list: List[int],
+    frequency: int,
+    cell_bit: int,
+):
     """
     remember to add batch 1 for x_test
     """
     layer_data = get_layer_data(model, x_test)
-    
-    # results = joblib.Parallel(n_jobs=32)(
-    #     joblib.delayed(run_layer)(layer, x_dim, y_dim, frequency) for layer in layer_data
-    # )
+    assert (
+        len(layer_data) == len(x_dim_list) == len(y_dim_list)
+    ), f"len(layer_data)={len(layer_data)}, len(x_dim_list)={len(x_dim_list)}, len(y_dim_list)={len(y_dim_list)}"
+
+    results = joblib.Parallel(n_jobs=32)(
+        joblib.delayed(run_layer)(layer, x_dim, y_dim, frequency)
+        for layer, x_dim, y_dim in zip(layer_data, x_dim_list, y_dim_list)
+    )
 
     # DEBUG
-    results = []
-    for layer in layer_data:
-        result = run_layer(layer, x_dim, y_dim, frequency)
-        results.append(result)
+    # results = []
+    # for layer, x_dim, y_dim in zip(layer_data, x_dim_list, y_dim_list):
+    #     result = run_layer(layer, x_dim, y_dim, frequency)
+    #     results.append(result)
 
     return results2ppa(results)
